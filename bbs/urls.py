819ffdbd19b0922465fb4387e129b61e26531568
TaleG <k.guogao@outlook.com>
2016-10-14 15:00:57 +0800

    kissme

---
+++ bbs/urls.py
@@ -0,0 +1,8 @@
+
+from django.conf.urls import url,include
+from bbs import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^category/(\d+)$', views.category),
+]
---
+++ bbs/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,HttpResponseRedirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+
+from bbs import models
+# Create your views here.
+category_list = models.Category.objects.filter(set_as_top_menu=True).order_by('position_index')
+def index(request):
+    return render(request,'bbs/index.html',{'category_list':category_list})
+
+
+def category(request,id):
+    category_obj = models.Category.objects.get(id=id)
+    article_list = models.Article.objects.filter(category_id=category_obj.id,status='published')
+    return render(request,'bbs/index.html',{'category_list':category_list,
+                                            'category_obj':category_obj,
+                                            'article_list':article_list,
+                                            })
+
+def acc_login(request):
+    if request.method == 'POST':
+        print(request.POST)
+        user = authenticate(username = request.POST.get('username'),
+                            password = request.POST.get('password'),
+                            )
+        if user is not None:
+            login(request,user)
+            return HttpResponseRedirect('/bbs')
+        else:
+            login_err = "Wrong username or password!"
+            return render(request, 'login.html', {'login_err':login_err})
+    return render(request, 'login.html')
+
+def acc_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/bbs')
